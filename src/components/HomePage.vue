1447010fca5be5add25d26f6762b1458543d21e3
Cam Murphy <CamMurphy195@gmail.com>
2019-03-17 21:25:37 -0500

    adding files

---
+++ src/components/HomePage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="homepage">
+    <div class="hero-image">
+        <div class="hero-text">
+          <h1> Which Horror Villian Are YOu?</h1>
+        </div>
+        <div >
+          <form action= "/Question1" >
+            <input class="button" type="submit" value="Get Started" />
+          </form>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'HomePage',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hero{
+    height: 100vh;
+    width: 100%
+  }
+  .hero-image{
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../assets/horror-banner.png");
+    display:block;
+    height: 100%;
+    width: 100%;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;  
+    background-size: contain;
+    position: absolute;
+    background-repeat: no-repeat;
+  }  
+
+  .button{
+    margin-top: 250px;  
+    padding: 20px 50px;
+    background-color:white;
+    color: black;
+  }
+
+  .button:hover{
+    background-color:orange;
+  }
+  
+  .hero-text{
+    margin-top: 10%;
+    color:white;
+    text-shadow: -1px 0 red, 0 1px red, 1px 0 red, 0 -1px red;
+  }
+</style>
