0a962bdafcfd1cb8f9c1beec02f870110182ec56
Cameron Murphy <cmurphy@onenorth.com>
2019-03-22 20:03:22 -0500

    added question page

---
+++ src/views/Question.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="question">
+    <h1>
+
+      {{QuestionNumber}}
+
+    </h1>
+
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+          QuestionNumber: 1
+      };
+    }
+  }
+
+</script>
