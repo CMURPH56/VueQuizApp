bffc783b2f12f5b2d7666825d0e021cf6d00ad9c
Cam Murphy <CamMurphy195@gmail.com>
2019-03-24 20:45:34 -0500

    added catch all to fix routing issue

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname + "/dist/index.html")
+});
 
-var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+ port);
+console.log('server started.. ')
