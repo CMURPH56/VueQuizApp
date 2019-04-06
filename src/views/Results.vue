8197c7fd631210ded7a31cb59e5e891e955094f4
Cam Murphy <CamMurphy195@gmail.com>
2019-04-06 18:57:47 -0500

    got svg pie chart to show up, I have to figure out how to plug in values

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -4,32 +4,97 @@
 
     <div>
         Most Popular Answers
-        <ul>
+        <ul class ="response-results">
             <li v-for="response in Responses">
                 {{response.Name}}  {{response.Count}}  
             </li>
         </ul>
+        <div>
+        <svg class ="pie">
+          <circle
+            v-for= "item in dataObjects"
+            v-bind:style="{strokeDashArray: `${item.RelativeSize} ${circleLength}`, strokeDashoffset: item.offset}"
+            r="25%"
+            cx="50%"
+            cy="50%"
+            />
+        </svg>
+        </div>
 
     </div>
     
     </div>
 </template>
 
-<style>
+<style lang="scss">
 .result{
     margin-top: 10%;
     color: white;
        text-shadow: -1px 0 red, 0 1px red, 1px 0 red, 0 -1px red;
 }
-
+.response-results{
+  list-style-type: none;
+}
+.pie circle {
+  fill: none;
+  stroke-width: 32px;
+  transition: stroke-dasharray 0.3s ease-in-out, stroke-dash-offset 0.3s ease-in-out;
+}
+$colors: red, yellow, cyan, blue, green,black, white, grey, purple;
+@for $i from 1 through length($colors){
+  .pie circle:nth-child(#{$i}){
+    stroke: nth($colors, $i);
+  }
+}
 </style>
+
 <script>
   import {db} from '../../firebaseConfig'
-export default{
+  import { setInterval } from 'timers';
+  export default{
+    mounted(){
+      setTimeout(() => {this.hasMounted = true}, 0);
+      setInterval(()=> {this.shuffle(this.data)}, 1000);
+    }, 
     data(){
       return {
        Responses: [],
+       data: [10,20,40,50],
+       circleLength: 371.9451599,
+       hasMounted: false,
       };
+    },
+    methods: {
+      shuffle(data){
+        let dataCopy = data.slice();
+        let temp;
+        let index;
+        let randomIndex;
+
+        for(index = 0; index < dataCopy.length; index++){
+          randomIndex = Math.floor(Math.random() * index);
+          temp = dataCopy[index]
+          dataCopy[index]= dataCopy[randomIndex]
+          dataCopy[randomIndex] = temp
+        }
+        this.data = dataCopy
+       
+      }
+    },
+    computed: {
+      dataTotal(){
+        return this.data.reduce((previous,current) => previous + current);
+      },
+      dataObjects(){
+        let startingPoint = 0;
+
+        return this.data.map(item =>{
+          let relativeSize = ((item /this.dataTotal) * this.circleLength)
+          let dataObject = { relativeSize: this.hasMounted ? relativeSize: 0, offset: -startingPoint}
+          startingPoint += relativeSize;
+          return dataObject
+        })
+      }
     },
     created(){
       db.collection('Responses').get().then((querySnapshot) =>{
